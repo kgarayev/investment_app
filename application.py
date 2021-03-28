d59344cd5048d8acc54f5c23d5236de4c65edcb1
kgarayev <kenangarayev@gmail.com>
2021-03-28 18:58:23 +0000

    added Procfile

--- application.py
+++ application.py
@@ -36,7 +36,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+# db = SQL("sqlite:///finance.db")
+db = SQL(os.getenv("DATABASE_URL"))
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
